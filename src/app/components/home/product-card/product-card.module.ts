90eab0b4b4cd2964f1983c3a8d7ce3343a39a337
Irving Campos <irving.campos@dacodes.com.mx>
2020-06-01 21:58:59 -0500

    Fihished home page, Finished header and add redirect file to netlify

--- src/app/components/home/product-card/product-card.module.ts
+++ src/app/components/home/product-card/product-card.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card.component';
+import { TitleBarModule } from 'src/app/shared/components/title-bar/title-bar.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [ProductCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    TitleBarModule
   ],
   exports: [
     ProductCardComponent
---
+++ src/app/core/interfaces/card-item.ts
@@ -0,0 +1,6 @@
+export interface ICardItem {
+  id?: number;
+  image?: string;
+  title?: string;
+  content?: string;
+}
